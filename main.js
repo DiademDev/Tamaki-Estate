e298da44c605f4a8ea18f7b96a974bef61d8c700
DiademDev <118876421+DiademDev@users.noreply.github.com>
2022-11-23 20:10:12 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+import * as THREE from '../../libs/three.js-r132/build/three.module.js';
+
+const scene = new THREE.SCENE()
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    innerWidth / innerHeight,
+    0.1,
+    1000
+)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.MeshBasicMaterial({
+        color: 0xff0000
+    })
+)
+
+function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+animate()
