52b9e12b9c2858a138728fca6e19114352f57cda
DiademDev <118876421+DiademDev@users.noreply.github.com>
2022-11-26 12:17:02 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-import * as THREE from 'https://ajax.googleapis.com/ajax/libs/threejs/r84/three.min.js';
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 
-const scene = new THREE.SCENE()
-const camera = new THREE.PerspectiveCamera(
-    75, 
-    innerWidth / innerHeight,
-    0.1,
-    1000
-)
-
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 )
 const renderer = new THREE.WebGLRenderer()
+
 renderer.setSize(innerWidth, innerHeight)
+renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.SphereGeometry(2, 20, 20),
     new THREE.MeshBasicMaterial({
         color: 0xff0000
     })
 )
+
+scene.add(sphere)
+
+camera.position.z = 10
 
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
 }
 animate()
+
