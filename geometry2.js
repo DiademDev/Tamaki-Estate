f1e7b177f46289a632d674c7271df0768df79f5b
DiademDev <118876421+DiademDev@users.noreply.github.com>
2023-05-17 13:45:27 +1200

    Add files via upload

--- geometry2.js
+++ geometry2.js
@@ -13,7 +13,7 @@ function init() {
 
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.z = 40;
+	camera.position.z = 30;
   	camera.position.y = 10;
 
 	renderer = new THREE.WebGLRenderer({ antialias: true });
