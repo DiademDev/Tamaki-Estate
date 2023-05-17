3e644f27ffc11bb0b7dee0aa1a77e501dbc57f7b
DiademDev <118876421+DiademDev@users.noreply.github.com>
2023-05-17 17:11:52 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,7 @@
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4Zjk5N2RlYS0zMGY2LTQxNWQtYjAwMy1iYWUyODI4ODY5YTUiLCJpZCI6MTE3OTUzLCJpYXQiOjE2NzA3Mzk4MTl9.k3I9be0G6cm7S9-U3lYsvSaUZ6mKVf0Capzojy3RZAU";
 Cesium.GoogleMaps.defaultApiKey = "AIzaSyA1au3L6n6ZZvFqojyNMfB27DiGHLAX7h8";
 
-// Camera settings
-const camDestination = Cesium.Cartesian3( 174.86123170659084, -36.896425016735904, 500 );
-const camHeading = 5.647630456593914;
-const camPitch = -0.5092018326918875;
-const camRoll = 0.00011791820395057329;
+
 
 async function main() {
 
@@ -28,98 +24,80 @@ async function main() {
   try {
     const tileset = await Cesium.createGooglePhotorealistic3DTileset();
     viewer.scene.primitives.add(tileset);
-    } catch (error) {
+  } catch (error) {
     console.log(`Error loading Photorealistic 3D Tiles tileset.\n${error}`);
   }
 
-  // Remove Cesium logo
-  viewer._cesiumWidget._creditContainer.style.display = "none";
+// Remove Cesium logo
+viewer._cesiumWidget._creditContainer.style.display = "none";
 
-  // Import data source file
-  const dataSourcePromise = Cesium.CzmlDataSource.load("data.czml");
-  viewer.dataSources.add(dataSourcePromise);
+// Import data source file
+const dataSourcePromise = Cesium.CzmlDataSource.load("data.czml");
+viewer.dataSources.add(dataSourcePromise);
 
-  // Create a custom InfoBox in javascript
-  const container = document.getElementById("cesiumContainer");
-  const infoBox = document.createElement("div");
-  const topDiv = document.createElement("div");
-  const botDiv = document.createElement("div");
-  const rightDiv = document.createElement("div");
-  topDiv.classList.add("top-div");
-  botDiv.classList.add("bot-div");
-  rightDiv.classList.add("rightSide-div")
-  infoBox.classList.add("custom-infobox");
-  infoBox.appendChild(topDiv);
-  infoBox.appendChild(botDiv);
-  infoBox.appendChild(rightDiv);
-  container.appendChild(infoBox);
+// Create a custom InfoBox in javascript
+const container = document.getElementById("cesiumContainer");
+const infoBox = document.createElement("div");
+const topDiv = document.createElement("div");
+const botDiv = document.createElement("div");
+const rightDiv = document.createElement("div");
+topDiv.classList.add("top-div");
+botDiv.classList.add("bot-div");
+rightDiv.classList.add("rightSide-div")
+infoBox.classList.add("custom-infobox");
+infoBox.appendChild(topDiv);
+infoBox.appendChild(botDiv);
+infoBox.appendChild(rightDiv);
+container.appendChild(infoBox);
 
-  // Custom infoBox logic
-  viewer.selectedEntityChanged.addEventListener((selectedEntity) => {
+// Custom infoBox logic
+viewer.selectedEntityChanged.addEventListener((selectedEntity) => {
 
-    if (selectedEntity === undefined || selectedEntity.name === "TamakiModel") {
-      // No entity is currently selected or TamakiModel entity is selected
-      if (infoBox.classList.contains("open")) {
-        infoBox.classList.remove("open");
-        infoBox.classList.add("close");
-      }
-    } else if (selectedEntity instanceof Cesium.Entity) {
-      // An entity has been selected
-      if (infoBox.classList.contains("close")) {
+  if (selectedEntity === undefined || selectedEntity.name === "TamakiModel") {
+    // No entity is currently selected or TamakiModel entity is selected
+    if (infoBox.classList.contains("open")) {
+      infoBox.classList.remove("open");
+      infoBox.classList.add("close");
+    }
+  } else if (selectedEntity instanceof Cesium.Entity) {
+    // An entity has been selected
+    if (infoBox.classList.contains("close")) {
+      infoBox.classList.remove("close");
+      infoBox.classList.add("open");
+    } else if (infoBox.classList.contains("open")) {
+      // Close the infoBox first before reopening it with updated information
+      infoBox.classList.remove("open");
+      infoBox.classList.add("close");
+      setTimeout(() => {
         infoBox.classList.remove("close");
         infoBox.classList.add("open");
-      } else if (infoBox.classList.contains("open")) {
-        // Close the infoBox first before reopening it with updated information
-        infoBox.classList.remove("open");
-        infoBox.classList.add("close");
-        setTimeout(() => {
-          infoBox.classList.remove("close");
-          infoBox.classList.add("open");
 
-          // Split Description into parts
-          var description = selectedEntity.description.getValue();
-          var parts = description.split("<hr>");
-          topDiv.innerHTML = parts[0];
-          botDiv.innerHTML = parts[1];
-          rightDiv.innerHTML = parts[2];
-        }, 300);
-        return;
-      } else {
-        infoBox.classList.add("open");
-      }
+        // Split Description into parts
         var description = selectedEntity.description.getValue();
         var parts = description.split("<hr>");
         topDiv.innerHTML = parts[0];
         botDiv.innerHTML = parts[1];
         rightDiv.innerHTML = parts[2];
+      }, 300);
+      return;
+    } else {
+      infoBox.classList.add("open");
     }
-  });
+      var description = selectedEntity.description.getValue();
+      var parts = description.split("<hr>");
+      topDiv.innerHTML = parts[0];
+      botDiv.innerHTML = parts[1];
+      rightDiv.innerHTML = parts[2];
+  }
+});
 
-  infoBox.addEventListener('click', (event) => {
+infoBox.addEventListener('click', (event) => {
 
-    if (event.target === infoBox) {
-        console.log("YES")
-    }
-  });
+  if (event.target === infoBox) {
+      console.log("YES")
+  }
+});
 
-  viewer.scene.camera.flyTo({
-    destination: Cesium.Cartesian3.fromDegrees(
-      camDestination
-    ),
-    orientation: {
-      heading: camHeading,
-      pitch: camPitch,
-      roll: camRoll
-    }
-  });
-}
-
-main();
-
-
-
-
-/* CODE SNIPPETS
   viewer.scene.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(
       174.86123170659084,
@@ -132,6 +110,15 @@ main();
       roll: 0.00011791820395057329
     }
   });
+}
+
+main();
+
+
+
+
+/* CODE SNIPPETS
+-36.89072172745562, 174.85554813871818
 
 // Create a Viewer with terrain - remove widgets
 const viewer = new Cesium.Viewer("cesiumContainer", {
