93b6baf1ca146db2d82db6cbd3195cf821a8a6f1
DiademDev <118876421+DiademDev@users.noreply.github.com>
2024-11-26 13:21:31 +1300

    Update DIR_01.js

--- DIR_01.js
+++ DIR_01.js
@@ -13,7 +13,7 @@ function init() {
 
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.z = 30;
+	camera.position.z = 20;
   	camera.position.y = 10;
 
 	renderer = new THREE.WebGLRenderer({ antialias: true });
