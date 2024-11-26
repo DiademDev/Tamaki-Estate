ce36d0b23cc31049586b4a3d4dbaf1e24c5c5e1b
DiademDev <118876421+DiademDev@users.noreply.github.com>
2024-11-26 15:29:39 +1300

    Update DIR_01.js

--- DIR_01.js
+++ DIR_01.js
@@ -17,6 +17,7 @@ function init() {
   	camera.position.y = 7;
 
 	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
